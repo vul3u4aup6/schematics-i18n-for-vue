fa706136a271cb1b524b9dafb2899ed302a1a66b
Tom <tom@phoenix-5.com>
2020-12-29 18:28:17 +0800

    modify index for lb

--- src/schematics-i18n-for-vue/index.ts
+++ src/schematics-i18n-for-vue/index.ts
@@ -4,27 +4,34 @@ import * as ts from 'typescript';
 
 export function schematicsI18nForVue(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    let [type, key] = _options.key.split('.');
+    let [type,key] = _options.key.split('.');
     if(!type){
       return
     }
-    let { tw } = _options;
-    const checkHasType = tsquery.query<ts.ObjectLiteralExpression>(tree.read('./src/lang/i18n/tw.js')!.toString(),`PropertyAssignment[name.name=${type}]`).pop()
-    if(checkHasType){
-      const getFinalProperty = tsquery.query<ts.ObjectLiteralExpression>(tree.read('./src/lang/i18n/tw.js')!.toString(),`PropertyAssignment[name.name=${type}]>ObjectLiteralExpression>PropertyAssignment`).pop()!
-      const mainJsRecorder = tree.beginUpdate('./src/lang/i18n/tw.js');
-      mainJsRecorder.insertLeft(getFinalProperty.end, `,\n\t\t${key}:"${tw}"`);
+    let option = _options;
+    let optionKey=Object.keys(option)
+    optionKey.shift()
+    optionKey.forEach(lang=>{
+      let langUrl=`./src/assets/lang/${lang}.js`
+      const checkHasType = tsquery.query<ts.ObjectLiteralExpression>(tree.read(langUrl)!.toString(),`ObjectLiteralExpression> PropertyAssignment[name.name=${type}]`).pop()
+      
+      if(checkHasType){
+        const getFinalProperty = tsquery.query<ts.ObjectLiteralExpression>(tree.read(langUrl)!.toString(),`PropertyAssignment[name.name=${type}]>ObjectLiteralExpression>PropertyAssignment`).pop()!
+        const mainJsRecorder = tree.beginUpdate(langUrl);
+        mainJsRecorder.insertLeft(getFinalProperty.end, `,\n\t\t${key}:"${option[lang]}"`);
+        tree.commitUpdate(mainJsRecorder);
+        return
+      }
+      const mainJsRecorder = tree.beginUpdate(langUrl);
+      const getFinalObjectLiteral = tsquery.query<ts.ObjectLiteralExpression>(tree.read(langUrl)!.toString(),'ObjectLiteralExpression > PropertyAssignment').pop()!
+      if(type && key){
+        mainJsRecorder.insertLeft(getFinalObjectLiteral.end, `,\n\t${type}:{\n\t\t${key}:"${option[lang]}"\n\t}`);
+      }else{
+        mainJsRecorder.insertLeft(getFinalObjectLiteral.end, `,\n\t${type}:"${option[lang]}"`);
+      }
+        
       tree.commitUpdate(mainJsRecorder);
-      return
-    }
-    const getFinalObjectLiteral = tsquery.query<ts.ObjectLiteralExpression>(tree.read('./src/lang/i18n/tw.js')!.toString(),'ObjectLiteralExpression').pop()!
-    const mainJsRecorder = tree.beginUpdate('./src/lang/i18n/tw.js');
-    if(type && key){
-      mainJsRecorder.insertLeft(getFinalObjectLiteral.end, `,\n\t${type}:{\n\t\t${key}:"${tw}"\n\t}`);
-    }else{
-      mainJsRecorder.insertLeft(getFinalObjectLiteral.end, `,\n\t${type}:"${tw}"`);
-    }
-    tree.commitUpdate(mainJsRecorder);
+    })
     return tree;
   };
 }
